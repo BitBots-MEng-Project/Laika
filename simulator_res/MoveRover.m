d36b3d5b99bed7beeb3ecc85464ebdcb4235dddd
hannah07929 <51329242+hannah07929@users.noreply.github.com>
2021-02-19 14:19:32 +0000

    with lidar data

--- simulator_res/MoveRover.m
+++ simulator_res/MoveRover.m
@@ -7,20 +7,23 @@ if (clientID>-1) %If the client ID is greater than -1, then the connection is su
     
     %Handle
     [returnCode, Left_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor', sim.simx_opmode_blocking);
-    [returnCode, Front_Sensor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor5', sim.simx_opmode_blocking);
+    [returnCode, Lidar]=sim.simxGetObjectHandle(clientID,'Lidar', sim.simx_opmode_blocking);
     [returnCode, camera]=sim.simxGetObjectHandle(clientID,'Vision_sensor', sim.simx_opmode_blocking);
     
+    
     %other code
-    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0.1,sim.simx_opmode_blocking);
-    [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Front_Sensor,sim.simx_opmode_streaming);
+    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0.5,sim.simx_opmode_blocking);
+    [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Lidar,sim.simx_opmode_streaming);
     [returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_streaming);
     
-    for i=1:50
-        [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Front_Sensor,sim.simx_opmode_buffer);
+    for i=1:100 %the code will run for 10 seconds 
+        
+        [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Lidar,sim.simx_opmode_buffer);
         [returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_buffer);
+        
         imshow(image)
         disp(norm(detectedPoint));
-        pause(0.1);
+        pause(0.1); %A command will be executed every 0.1 seconds 
     end 
     
     [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0,sim.simx_opmode_blocking); %Turn off actuator
