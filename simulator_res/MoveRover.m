b1cfc807e1f25728a9ed10ce6f5500e105aee0ad
hannah07929 <51329242+hannah07929@users.noreply.github.com>
2021-02-24 21:10:16 +0000

    localising robot

--- simulator_res/MoveRover.m
+++ simulator_res/MoveRover.m
@@ -5,29 +5,48 @@ clientID=sim.simxStart('127.0.0.1',19999,true,true,5000,5); %Create connection u
 if (clientID>-1) %If the client ID is greater than -1, then the connection is succesful
     disp('connected')
     
-    %Handle
-    [returnCode, Left_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor', sim.simx_opmode_blocking);
-    [returnCode, Lidar]=sim.simxGetObjectHandle(clientID,'Lidar', sim.simx_opmode_blocking);
-    [returnCode, camera]=sim.simxGetObjectHandle(clientID,'Vision_sensor', sim.simx_opmode_blocking);
+    %Handles
+    [returnCode, Left_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor', sim.simx_opmode_blocking); %left motor handle
+    [returnCode, Right_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor', sim.simx_opmode_blocking); %right motor handle
+    [returnCode, Lidar]=sim.simxGetObjectHandle(clientID,'Lidar', sim.simx_opmode_blocking); %lidar handle
+    [returnCode, Odometry]=sim.simxCreateDummy(clientID,0.01,[],sim.simx_opmode_blocking) %dummy handle
+    [returnCode,Robot_position]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx',sim.simx_opmode_blocking); %robot handle
     
+    retreating = 0;
+    %[returnCode, camera]=sim.simxGetObjectHandle(clientID,'Vision_sensor', sim.simx_opmode_blocking);
     
-    %other code
-    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0.5,sim.simx_opmode_blocking);
+    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,1,sim.simx_opmode_blocking); %spin left motor at 1 m/s
+    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Right_Motor,1,sim.simx_opmode_blocking); %spin right motor at 1 m/s
+    %[returnCode]=sim.simxSetObjectPosition(clientID,Robot_position,-1,[1,2,3,6],sim.simx_opmode_oneshot); %robot position
+    
+    while (true)    
     [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Lidar,sim.simx_opmode_streaming);
-    [returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_streaming);
-    
-    for i=1:100 %the code will run for 10 seconds 
+    %[returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_streaming);
         
-        [returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Lidar,sim.simx_opmode_buffer);
-        [returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_buffer);
+        %[returnCode,detectionState,detectedPoint,~,~]=sim.simxReadProximitySensor(clientID,Lidar,sim.simx_opmode_buffer);
+        %[returnCode,resolution,image]=sim.simxGetVisionSensorImage2(clientID,camera,1,sim.simx_opmode_buffer);
         
-        imshow(image)
-        disp(norm(detectedPoint));
+%         if (detectionState==1) && (norm(detectedPoint) < 0.4) && (retreating==0)
+%             disp(detectedPoint)
+%             retreating = 1;
+%             [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,-0.5,sim.simx_opmode_blocking); 
+%             [returnCode]=sim.simxSetJointTargetVelocity(clientID,Right_Motor,-0.5,sim.simx_opmode_blocking); 
+%         %elseif (retreating == 1)
+% %             [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0,sim.simx_opmode_blocking); %Turn off left actuator
+% %             [returnCode]=sim.simxSetJointTargetVelocity(clientID,Right_Motor,0,sim.simx_opmode_blocking); %Turn off right actuator
+%         end
+        disp(detectedPoint)
+        if (detectionState==0) && (retreating==1)
+            retreating =0;
+        end
+        
+        %imshow(image)
+%         disp(norm(detectedPoint));
         pause(0.1); %A command will be executed every 0.1 seconds 
     end 
     
-    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0,sim.simx_opmode_blocking); %Turn off actuator
-   
+    [returnCode]=sim.simxSetJointTargetVelocity(clientID,Left_Motor,0,sim.simx_opmode_blocking); %Turn off left actuator
+   [returnCode]=sim.simxSetJointTargetVelocity(clientID,Right_Motor,0,sim.simx_opmode_blocking); %Turn off right actuator
     sim.simxFinish(-1); %Close any unopened connections 
 end
 
